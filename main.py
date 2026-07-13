b504d4d9cfb56a2dc8c209db09df25c6a2b11907
Albin34 <129714787+Albin34@users.noreply.github.com>
2026-07-13 17:09:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ from tenacity import (
 st.set_page_config(
     page_title="RFQ Data Processor",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="collapsed",
 )
 
 st.markdown(
